beab80ce3b45175900d627398be8046f69a5fe7c
HenryYenHa <henryha@live.ca>
2023-11-01 17:54:37 -0600

    Up to Coding Practice

---
+++ js-stuff.js
@@ -0,0 +1,92 @@
+{
+  [
+    {
+      name: "Green Sock",
+      description: "JS Animations",
+      link: "https://gsap.com/",
+      tags: ["animation","javascript", "animate", "js", "frontend"]
+    }
+    {
+      name: "Can I Use",
+      description: "Check I use this on which browsers?",
+      link: "https://caniuse.com/",
+      tags: ["frontend", "css","html"]
+    }
+    {
+      name: "CodinGame",
+      description: "Play games and refine your coding abilities",
+      link: "https://www.codingame.com/",
+      tags: ["practice", "javascript", "js"]
+
+    }
+    {
+      name: "Codewars",
+      description: "Write code to solve problems; compare solutions with peers.",
+      link: "https://www.codewars.com",
+      tags: ["practice", "javascript", "js"]
+    }
+
+  ]
+}
+
+CSS:
+https://css-tricks.com/snippets/css/a-guide-to-flexbox/
+https://css-tricks.com/snippets/css/complete-guide-grid/
+https://www.youtube.com/watch?v=Icf5D3fEKbM
+https://www.codecademy.com/learn/learn-css
+https://www.codecademy.com/learn/learn-intermediate-css
+
+ExpressJS:
+https://www.youtube.com/watch?v=SccSCuHhOw0
+
+Font-Related Stuff:
+https://fonts.google.com/
+https://heyreliable.com/ultimate-google-font-pairings/
+
+Github:
+https://www.codecademy.com/learn/learn-git-introduction
+https://dev.to/suchintan/git-and-github-tutorial-beginner-to-advanced-part-1-32m6
+https://dev.to/suchintan/git-and-github-tutorial-beginner-to-advanced-part-2-2h5c
+https://dev.to/suchintan/git-and-github-tutorial-beginner-to-advanced-part-3-1i09
+
+HTML:
+https://www.markdownguide.org/cheat-sheet/
+https://www.youtube.com/watch?v=qz0aGYrrlhU
+https://www.codecademy.com/learn/learn-html
+
+InceptionU's Code Repository:
+https://github.com/EvolveU-FSD
+
+JavaScript:
+(Beginner) https://www.youtube.com/watch?v=W6NZfCO5SIk&t=1s
+(Beginner) https://www.codecademy.com/learn/introduction-to-javascript
+(Intermediate)https://www.youtube.com/watch?v=Bv_5Zv5c-Ts
+(Intermediate)https://www.freecodecamp.org/news/search/?query=Javascript
+(Advanced)https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/
+
+JavaScript Libraries:
+https://lodash.com/
+
+Linux (Console?) Commands:
+https://www.guru99.com/linux-commands-cheat-sheet.html
+
+NodeJS:
+https://nodejs.dev/en/learn/
+https://www.youtube.com/watch?v=TlB_eWDSMt4
+
+Palette Picker:
+https://coolors.co/image-picker
+
+Sample Code/Templates:
+https://freefrontend.com/
+https://getcssscan.com/css-buttons-examples
+
+Sharing/Testing/Collaborating Code:
+https://codepen.io/pen/
+https://jsfiddle.net/
+
+Tony's List of Free APIs:
+https://gist.github.com/acidtone/673dfc5c11ce06e9e8cd6ce33609eb3c
+
+VS Code Shortcuts:
+https://www.youtube.com/watch?v=dI34jrEtmB0
